b27ddba68f61e171a777c13950701d0a3b3218a8
John Britton <public@johndbritton.com>
2020-12-26 03:40:57 -0500

    sketch out a basic working mvp

---
+++ Horologe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Horologe
+//
+//  Created by John Britton on 12/25/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Open \"Notification Center\" and click on \"Edit Widgets\" to install.")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Horologe/HorologeApp.swift
@@ -0,0 +1,17 @@
+//
+//  HorologeApp.swift
+//  Horologe
+//
+//  Created by John Britton on 12/25/20.
+//
+
+import SwiftUI
+
+@main
+struct HorologeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
